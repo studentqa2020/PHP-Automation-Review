83b181b68ae60150ad05faf28e06da24b08c4d52
misla <misla@DESKTOP-BCIB9AD>
2020-11-17 16:59:53 -0500

    php.final.run

---
+++ phpselenium/src/test/java/phpselenium/phpTestRunFinal.java
@@ -0,0 +1,16 @@
+package phpselenium;
+
+import com.generic.cod.PhpMainLogin;
+
+public class phpTestRunFinal {
+	
+	public static void main(String[] args) throws Throwable {
+		PhpMainLogin.getlogin();
+	}
+	
+	
+	
+	
+	
+
+}
